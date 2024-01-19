4cbef4a033e0066ca20e5cf4a614b3b2db378e9c
bottlewinds <sandrillamel@gmail.com>
2024-01-18 19:01:04 -0500

    update text

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Holiwi q pasooo te amo mi amooor</Text>
+      <Text>Esto lo arregle en un hotfix</Text>
       <StatusBar style="auto" />
     </View>
   );
